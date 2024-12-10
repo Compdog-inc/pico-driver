10e08366bc12b537af59fe3cbb6b5e1f1f3eaaba
Compdog Inc <compdog.inc@gmail.com>
2024-12-10 18:01:38 -0500

    Add webapp build to publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+# Initialize NodeJS project
+cd ./webapp
+npm ci          # install packages
+npm run build   # build static webapp
+
+cd ../
+
+# Publish .NET project
 dotnet publish DriverStation.csproj -c Release -r osx-x64 --sc -o bin/Release/net8.0/publish/osx-x64
